14b2ad73c0d9e9a4ee9f9e4679dd35faeb418259
nguyenlyminhman <nguyenlyminhman@gmail.com>
2018-05-04 01:36:46 +0700

    Finish login function

--- apps/controllers/admin.js
+++ apps/controllers/admin.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let router = express.Router();
 let userModel = require("../model/users");
+let utils = require("../utils/encrypt")
 
 router.get("/", (req, res) => {
     res.json({ "message": "This is admin page" });
@@ -10,21 +11,56 @@ router.get("/signup", (req, res) => {
     res.render("signup", { data: {} });
 });
 
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
     var user = req.body;
+    var hashePassword = utils.hashPassword(user.password);
     user = {
         email: user.email,
-        password: user.password,
+        password: hashePassword,
         first_name: user.firstname,
         last_name: user.lastname
     }
-    var result = userModel.addUser(user);
-    if(!result){
-        res.render("signup", { data: {error:"Error occurs!"} });
-    }else{
-        res.render("signup", { data: {success:"Added successfully!"} });
-    }
-})
+    console.log(user);
 
+    userModel.addUser(user).then(data => {
+        res.render("signup", { data: { success: "Added successfully!" } });
+    }).catch(err => {
+        res.render("signup", { data: { error: "Email was existed!" } });
+    })
+
+    // if(!result){
+    //     res.render("signup", { data: {error:"Error occurs!"} });
+    // }else{
+    //     res.render("signup", { data: {success:"Added successfully!"} });
+    // }
+});
+
+router.get("/signin", (req, res) => {
+    res.render("signin", { data: {} });
+});
+router.post("/signin", (req, res) => {
+    var params = req.body;
+    if (params.email) {
+        userModel.getUserByEmail(params.email).then(data => {
+            var user = data[0];
+            var status = utils.comparePassword(params.password, user.password);
+            if(!status){
+                res.render("signin", { data: { error: "Email was not existed!" } });
+            }else{
+                req.session.user = user;
+                res.redirect("/admin/")
+            }
+        }).catch(err => {
+            res.render("signin", { data: { error: "Email was not existed!" } });
+        })
+    }
+
+
+    // if(!result){
+    //     res.render("signup", { data: {error:"Error occurs!"} });
+    // }else{
+    //     res.render("signup", { data: {success:"Added successfully!"} });
+    // }
+});
 module.exports = router;
 
---
+++ apps/utils/encrypt.js
@@ -0,0 +1,20 @@
+let config = require("config");
+let bcrypt = require("bcrypt");
+
+module.exports = {
+
+    hashPassword(password) {
+        var saltRound = config.get("salt");
+
+        var saltt = bcrypt.genSaltSync(saltRound);
+        var hash = bcrypt.hashSync(password, saltt);
+        return hash;
+    },
+
+    comparePassword(password, hash) {
+        return bcrypt.compareSync(password, hash);
+    }
+}
+
+
+
